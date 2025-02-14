e4098820549e653f069061b356e0896eeba54f0c
Dushime Beni <bdushime47@gmail.com>
2025-02-14 11:03:49 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+const { addDynamicIconSelectors } = require('@iconify/tailwind')
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [addDynamicIconSelectors()] ,
+}
+
